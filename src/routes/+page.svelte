e515cc46614cb827c2750d0c57f39c096c331cea
Connor <.>
2025-08-29 19:48:16 -0700

    Added funcionality to list

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,43 +1,48 @@
 <script>
-  let days = ["su", "m", "t", "w", "th", "f", "s"];
-  let events = [
-    {date: 19, eventName: "bday party"},
-    {date: 27, eventName: "soccer practice"},
-    {date: 31, eventName: "LAN party"},
-  ]
-
-  function hasEvent(day) {
-    return day == 18 || day == 27;
-  }
+	let days = ['su', 'm', 't', 'w', 'th', 'f', 's'];
+	let todos = $state([
+		{ date: '2025/08/25', eventName: 'bday party' },
+		{ date: '2025/08/27', eventName: 'soccer practice' },
+		{ date: '2025/08/29', eventName: 'LAN party' },
+		{ date: '2025/08/29', eventName: 'trip to SD' }
+	]);
 </script>
 
 <div>
-  <!-- calendar -->
-  <div class="calendar">
-    {#each days as day}
-      <div>{day}</div>
-    {/each}
-    {#each new Array(31).fill(0) as day, i}
-      <div>
-        {i+1}
-        {#if hasEvent(i+1)}
-          *
-        {/if}
-      </div>
-    {/each}
-  </div>
-
-
+	<!-- calendar -->
+	<div class="calendar">
+		{#each days as day}
+			<div>{day}</div>
+		{/each}
+		{#each new Array(31).fill(0) as day, i}
+			<div>
+				{i + 1}
+				{#each todos.filter((v) => v.date.split('/')[2] == i + 1) as v, i}
+					<div class="event">{v.eventName}</div>
+				{/each}
+			</div>
+		{/each}
+	</div>
 </div>
 
 <style>
-  .calendar {
-    display: grid;
-    grid-template-columns: repeat(7, 1fr);
-  }
+	.calendar {
+		display: grid;
+		grid-template-columns: repeat(7, 1fr);
+		grid-auto-rows: 60px;
+	}
 
-  .calendar > div {
-    background-color: skyblue;
-    margin: 1px;
+	.calendar > div {
+		background-color: skyblue;
+		margin: 1px;
+	}
+
+  .event {
+    font-size: 8px;
+    background-color: purple;
+    color: white;
+    padding: 2px 5px;
+    border-radius: 5px;
+    margin: 0.5px;
   }
 </style>
--- src/routes/list/+page.svelte
+++ src/routes/list/+page.svelte
@@ -1,21 +1,46 @@
 <script>
-	let todos = $state([
-		{ date: '2025/08/25', eventName: 'bday party' },
-		{ date: '2025/08/27', eventName: 'soccer practice' },
-		{ date: '2025/08/29', eventName: 'LAN party' }
-	]);
+	import { onMount } from "svelte";
+
+
+	let todos = $state([]);
+
+	onMount(() => {
+		if(localStorage.getItem("todos")) {
+			todos = JSON.parse(localStorage.getItem("todos"));
+		}
+	})
+
 	let newTodoText = $state('');
 	let newTodoDate = $state('');
-
+	let keyNumber = 0; 
+	// function setLocalStorageItem(eventObj) {
+	// 	// if(localStorage.getItem("keyCount") !== null) {
+	// 		// keyNumber = localStorage.getItem("keyCount");
+	// 	// }
+	// 	let objAr = { date: eventObj.date, eventName: eventObj.eventName };
+	// 	// localStorage.setItem(keyNumber, JSON.stringify(objAr));
+	// 	// keyNumber++;
+	// 	// localStorage.setItem("keyCount", keyNumber);
+	// 	todos.push(objAr);
+	// 	localStorage.setItem("todos", JSON.stringify(todos));
+	// }
 	function handleAdd() {
-		if (newTodoText.length > 0) {
+		if (newTodoText.length > 0 && newTodoDate.length > 0) {
 			const todoItem = {
 				date: newTodoDate,
 				eventName: newTodoText
 			};
+			// setLocalStorageItem(todoItem)
 			todos.push(todoItem);
+			localStorage.setItem("todos", JSON.stringify(todos));
 			newTodoText = '';
+			newTodoDate = '';
 		}
+	}
+
+	function handleDelete(index) {
+		todos.splice(index, 1);
+		localStorage.setItem("todos", JSON.stringify(todos))
 	}
 </script>
 
@@ -25,31 +50,25 @@
 	<input placeholder="todo name" bind:value={newTodoText} />
 	<input bind:value={newTodoDate} type="date" />
 	<button onclick={handleAdd}>add</button>
-    <div id="listboxes">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-    </div>
-    <!--
-	<ol>
-		{#each todos as v}
-			<li>{v.date} - {v.eventName}</li>
+	<div id="listboxes">
+		{#each todos as v, i}
+			<div>{v.date}</div>
+			<div>
+				{v.eventName}
+				<button onclick={() => handleDelete(i)}>x</button>
+			</div>
 		{/each}
-	</ol>-->
+	</div>
 </div>
 
 <style>
-#listboxes {
-    display: grid;
-    grid-template-rows: 7;
-    min-height: 65vh;
-    margin: 20px;
-}
-#listboxes > div {
-    border: 1px solid grey;
-}
+	#listboxes {
+		display: grid;
+		grid-template-columns: auto 1fr;
+		gap: 3px;
+	}
+	#listboxes > div {
+		background-color: #eee;
+		padding: 4px;
+	}
 </style>
